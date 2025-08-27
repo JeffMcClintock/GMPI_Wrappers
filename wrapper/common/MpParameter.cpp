247c1150b630b0766750e504cd659784a1920ae5
Jeff McClintock <jef@synthedit.com>
2025-08-27 16:41:41 +1200

    compiler error on linux

--- wrapper/common/MpParameter.cpp
+++ wrapper/common/MpParameter.cpp
@@ -83,13 +83,13 @@ bool MpParameter_base::setParameterRaw(gmpi::Field paramField, int32_t size, con
 		{
 // -ves fail			newRawValue = ToRaw4((int32_t)(0.5 + realWorld));
 			//newRawValue = ToRaw4((int32_t) static_cast<int32_t>(0.5 + realWorld));
-			newRawValue = ToRaw4(static_cast<int32_t>(round(realWorld)));
+			newRawValue = ToRaw4(static_cast<int32_t>(std::round(realWorld)));
 			break;
 		}
 		case gmpi::PinDatatype::Int64:
 		{
 			// -ves fail			newRawValue = ToRaw4((int64_t)(0.5 + realWorld));
-			newRawValue = ToRaw4(static_cast<int64_t>(round(realWorld)));
+			newRawValue = ToRaw4(static_cast<int64_t>(std::round(realWorld)));
 			break;
 		}
 		case gmpi::PinDatatype::Bool:
