ceebdbe3db146a1fd27ba9fae4bfbcdacad6b7fa
Jeff McClintock <jef@synthedit.com>
2025-09-17 15:07:05 +1200

    sync w SynthEditlib

--- wrapper/VST3/Controller_VST3.h
+++ wrapper/VST3/Controller_VST3.h
@@ -65,13 +65,13 @@ public:
 	inline char* data() { return storage_.data(); }
 	inline void clear() { writePos_ = 0; }
 	inline int freeSpace() override { return (int)(storage_.size() - writePos_); }
-	inline int totalSize() { return (int)storage_.size(); }
+	inline int totalSpace() override { return (int)storage_.size(); }
 	inline bool empty() { return writePos_ == 0; }
 
 	// IWriteableQue
 	void pushString(int p_length, const unsigned char* p_data) override
 	{
-		if (p_length <= totalSize() - writePos_)
+		if (p_length <= totalSpace() - writePos_)
 		{
 			memcpy(data() + writePos_, p_data, p_length);
 			writePos_ += p_length;
